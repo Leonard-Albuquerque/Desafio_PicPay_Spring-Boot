6614e6fb95ceb6b970a3b05c7fcd67795e200fe6
Leonard Albuquerque <jamesxalbuquerque@gmail.com>
2024-11-04 13:10:43 -0300

    Correção estrutural Transaction (Conflito de import)

--- picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/domain/transaction/Transaction.java
+++ picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/domain/transaction/Transaction.java
@@ -20,7 +20,7 @@ import java.time.LocalDateTime;
 
 
 
-public class transaction {
+public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/repositories/TransactionRepository.java
+++ picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.picpaySimplificado.picpaySimplificado.repositories;
 
-import jakarta.transaction.Transaction;
+import com.picpaySimplificado.picpaySimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
