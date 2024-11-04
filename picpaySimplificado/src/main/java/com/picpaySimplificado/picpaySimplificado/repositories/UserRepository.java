d7051535efbc845e6283a084881e6064e5ed47a7
Leonard Albuquerque <jamesxalbuquerque@gmail.com>
2024-11-04 13:13:00 -0300

    Organization

--- picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/repositories/UserRepository.java
+++ picpaySimplificado/src/main/java/com/picpaySimplificado/picpaySimplificado/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findById(Long id);
+    //
 }
